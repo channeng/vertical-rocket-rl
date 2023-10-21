bd5e3dbe7d63aba8e141d32f5ee960b18c847200
Trieu Le <trieu.lehong2510@gmail.com>
2023-10-21 13:01:50 +0800

    Explicitly load the VerticalRocket gym

--- gym/envs/__init__.py
+++ gym/envs/__init__.py
@@ -1,8 +1,9 @@
 from gym.envs.registration import register, make, spec
+import gym.envs.box2d.vertical_rocket
 
 register(
     id='VerticalRocket-v1',
-    entry_point='gym.envs.box2d:VerticalRocket',
+    entry_point='gym.envs.box2d.vertical_rocket:VerticalRocket',
     max_episode_steps=1000,
     reward_threshold=8,
 )
